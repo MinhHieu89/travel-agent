fc76322410771a03e734d34ace9d7957b4cb9024
Minh Hieu <minh.hieu.89@outlook.com>
2017-06-22 16:12:58 +0700

    FInish Large-Hero and Button blocks.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
-    nested = require('postcss-nested');
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import');
 
 gulp.task('default', function () {
     console.log('Hello');
@@ -11,7 +12,7 @@ gulp.task('default', function () {
 
 gulp.task('styles', function () {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([ autoprefixer, cssvars, nested ]))
+        .pipe(postcss([ cssImport, nested, cssvars ,autoprefixer ]))
         .pipe(gulp.dest('./app/temp/styles'));
 });
 
